0236f3bf0163ad93d251782a2c1a224478a686b9
Jimmie Butler <jimmiebtlr@gmail.com>
2015-01-22 18:04:56 -0600

    Set to version 0.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'elevatedevdesign:simple-blog',
   summary: 'Simple flexible blog for meteor',
-  version: '0.0.1-rc.1',
+  version: '0.0.1',
   git: 'https://github.com/ElevateDevelopmentAndDesign/meteor-simple-blog'
 });
 
