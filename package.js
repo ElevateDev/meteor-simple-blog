157d8962a3e826ca52f41a7108052abe8a28473a
Jimmie Butler <jimmiebtlr@gmail.com>
2015-01-25 13:14:52 -0600

    Bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'elevatedevdesign:simple-blog',
   summary: 'Simple flexible blog for meteor',
-  version: '0.0.2',
+  version: '0.0.3',
   git: 'https://github.com/ElevateDevelopmentAndDesign/meteor-simple-blog'
 });
 
