64849c7e3c31311a21e4fccacfabe3d811fdcfa4
Jimmie Butler <jimmiebtlr@gmail.com>
2015-01-27 15:42:19 -0600

    Fix publishedDate autovalue bug.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'elevatedevdesign:simple-blog',
   summary: 'Simple flexible blog for meteor',
-  version: '0.0.6',
+  version: '0.0.7',
   git: 'https://github.com/ElevateDevelopmentAndDesign/meteor-simple-blog'
 });
 
--- tests/client.js
+++ tests/client.js
@@ -18,16 +18,3 @@ Tinytest.add('Post - Published date should auto fill when published is set true'
   test.equal(!beforeClean.isAfter(post.publishedDate), true, 'Cleaned publishedDate value should be set to current time');
   test.equal(!afterClean.isBefore(post.publishedDate), true, 'Cleaned publishedDate value should be set to current time');
 });
-
-/* 
- * I'm not sure if this is an effective test
- */
-Tinytest.add('Post - Published date should not change after its already published', function (test) {
-  var schema = new SimpleSchema(Blog.postsSchema);
-  var userId = Random.id();
-  Meteor.userId = function(){ return userId; }
-  var originalMoment = moment().add(-1, 'days');
-  var post = {title: "Test", description: "test", body: "test", published: true, publishedDate: originalMoment};
-  schema.clean(post);
-  test.equal(moment(post.publishedDate).isSame(originalMoment), true, 'Published date should not change after initially set.');
-});
